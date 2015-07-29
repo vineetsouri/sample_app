134c6073a10782917053c0bf0f98ab04ee53c101
vineetsouri <vineetsouri72@gmail.com>
2015-07-29 11:54:24 +0530

    changed the html pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141111214811) do
+ActiveRecord::Schema.define(version: 20150727214811) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -41,13 +41,13 @@ ActiveRecord::Schema.define(version: 20141111214811) do
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
     t.string   "password_digest"
-    t.string   "remember_digest"
     t.boolean  "admin",             default: false
+    t.string   "remember_digest"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
-    t.string   "reset_digest"
-    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
